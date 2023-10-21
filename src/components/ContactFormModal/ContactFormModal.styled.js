97841fb9bd204211c16e48a0f48304811299c012
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-10-22 00:11:23 +0300

    footer

--- src/components/ContactFormModal/ContactFormModal.styled.js
+++ src/components/ContactFormModal/ContactFormModal.styled.js
@@ -2,6 +2,13 @@ import styled from 'styled-components';
 
 export const ModalOpenButton = styled.button`
   position: absolute;
-  bottom: 20px;
+  z-index: 120;
+  bottom: 80px;
   right: 20px;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  background-color: goldenrod;
 `;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,17 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <AppBar
+      position="static"
+      style={{
+        boxShadow:
+          '0px -2px 4px -1px rgba(0, 0, 0, 0.2), 0px -4px 5px 0px rgba(0, 0, 0, 0.14), 0px -1px 10px 0px rgba(0, 0, 0, 0.12) ',
+      }}
+    >
+      <Toolbar></Toolbar>
+    </AppBar>
+  );
 }
