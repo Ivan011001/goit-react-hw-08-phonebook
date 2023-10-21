6ce883340816b3092cdda76d007328e6c0011689
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-10-21 23:57:15 +0300

    homehage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,27 @@
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserIsLogged } from 'redux/auth/selectors';
+
 export default function HomePage() {
+  const navigate = useNavigate();
+  const isLogged = useSelector(selectUserIsLogged);
+
   return (
-    <div>
-      <h1>Here You Can Build Your Own Contact Book</h1>
+    <div
+      style={{
+        textAlign: 'center',
+        marginTop: 300,
+      }}
+    >
+      <h1 style={{ marginBottom: 20 }}>
+        Here You Can Build Your Own Contact Book
+      </h1>
+      {!isLogged && (
+        <Button variant="contained" onClick={() => navigate('/login')}>
+          Log In
+        </Button>
+      )}
     </div>
   );
 }
